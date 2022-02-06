236fd3a0fbcbd7e472335f1bcb339e8812395fb8
ParnThanatibordee <parn8946@gmail.com>
2022-02-06 15:35:32 +0700

    reserve(post)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pymongo import MongoClient
 from pydantic import BaseModel
 
@@ -18,7 +18,6 @@ collection = db["tables"]
 app = FastAPI()
 
 
-# TODO complete all endpoint.
 @app.get("/reservation/by-name/{name}")
 def get_reservation_by_name(name:str):
     pass
@@ -30,8 +29,14 @@ def get_reservation_by_table(table: int):
 
 
 @app.post("/reservation")
-def reserve(reservation : Reservation):
-    pass
+def reserve(reservation: Reservation):
+    find_previous_reserve = collection.find({"$and": [{"table": reservation.table_number},
+                                                      {"time": {"&gt": reservation.time}},
+                                                      {"time": {"&lt": reservation.time+1}}]})
+    if len(find_previous_reserve) == 0:
+        collection.insert_one(reservation)
+    else:
+        raise HTTPException(404, f"The table already reserved")
 
 
 @app.put("/reservation/update/")
