fd4bc0dced3d7aa3f2e6a99c8bcea76e909ca35a
ParnThanatibordee <parn8946@gmail.com>
2022-02-06 15:17:12 +0700

    add path of database

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 from pydantic import BaseModel
 
+
 class Reservation(BaseModel):
-    name : str
+    name: str
     time: int
     table_number: int
-    
+
+
 client = MongoClient('mongodb://localhost', 27017)
 
-# TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurant"]
 
-# TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["tables"]
 
 app = FastAPI()
 
@@ -23,19 +23,22 @@ app = FastAPI()
 def get_reservation_by_name(name:str):
     pass
 
+
 @app.get("reservation/by-table/{table}")
 def get_reservation_by_table(table: int):
     pass
+
 
 @app.post("/reservation")
 def reserve(reservation : Reservation):
     pass
 
+
 @app.put("/reservation/update/")
 def update_reservation(reservation: Reservation):
     pass
 
+
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
     pass
-
