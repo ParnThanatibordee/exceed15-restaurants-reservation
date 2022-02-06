551bc536183abf6da6b5d51a4ba869726ab68bed
yossvaris <dollaryossvaris@gmail.com>
2022-02-06 15:45:07 +0700

    delete

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Reservation(BaseModel):
 client = MongoClient('mongodb://localhost', 27017)
 
 # TODO fill in database name
-db = client["<put your database name>"]
+db = client["restaurant"]
 
 # TODO fill in collection name
-collection = db["<put your collection name>"]
+collection = db["tables"]
 
 app = FastAPI()
 
@@ -37,5 +37,9 @@ def update_reservation(reservation: Reservation):
 
 @app.delete("/reservation/delete/{name}/{table_number}")
 def cancel_reservation(name: str, table_number : int):
-    pass
+    query = {"name": name, "table_number": table_number}
+    collection.delete_many(query)
+    return{
+        "deleted"
+    }
 
